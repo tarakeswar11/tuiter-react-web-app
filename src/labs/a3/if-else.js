b461439a8ab64ad8aa7a490fa051d123333d5657
Tarakeswar Nallamothu <59277224+tarakeswar11@users.noreply.github.com>
2023-05-29 08:52:34 -0400

    v10

--- src/labs/a3/if-else.js
+++ src/labs/a3/if-else.js
@@ -3,15 +3,10 @@ import React from 'react';
 let true1 = true
     let false1 = false
     let false2 = true1 && false1
-    let true2 = true1 || false1
-    let true3 = !false2
+    
     let numberVariable = 123;
     let floatingPointNumber = 234.345;
-    let true4 = numberVariable === 123;
-    let true5 = floatingPointNumber !== 321.432;
-    let false3 = numberVariable < 100
-    let sortaTrue = '1' == 1  // never use this ==
-    let notTrue   = '1' === 1 // always use this ===
+    
 
 function IfElse() {
     console.log('If else');
