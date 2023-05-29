89ecde03bcb9b08907cf05e010c43e5abbc5da57
Tarakeswar Nallamothu <59277224+tarakeswar11@users.noreply.github.com>
2023-05-29 01:47:25 -0400

    v9

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import HelloWorld from "./labs/a3/hello-world";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
-
+import { Navigate } from 'react-router';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="container">
         <Routes>
-          <Route index element = {<Labs/>}/>
-          
+        <Route path="/"         element={<Navigate to="/labs"/>}/>
+          <Route path="/labs/*"   element={<Labs/>}/>
           <Route path="/hello"    element={<HelloWorld/>}/>
           <Route path="/tuiter/*" element={<Tuiter/>}/>
         </Routes>
