bc414a2269eb216dfe7a88d6113a6f3be234a569
Tarakeswar Nallamothu <59277224+tarakeswar11@users.noreply.github.com>
2023-06-10 02:03:56 -0400

    v5

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -17,7 +17,7 @@ export const logout = async () => {
     return response.data;
    };
 export const profile = async () => {
-    const response = await api.post(`${USERS_URL}`);
+    const response = await api.post(`${USERS_URL}/profile`);
     return response.data;
    };
 export const updateUser = async (user) => {
