983f57e929a7fa2070e89619167e6e8f608d1f6c
Tarakeswar Nallamothu <59277224+tarakeswar11@users.noreply.github.com>
2023-06-10 01:24:00 -0400

    v2

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const TUITS_API = 'http://localhost:4000/api/tuits';
+// const TUITS_API = 'http://localhost:4000/api/tuits';
+const TUITS_API = "https://tuiter-node-server-app-edyt.onrender.com/api/tuits"
+
+
+
 export const createTuit = async (tuit) => {const response = await axios.post(TUITS_API, tuit)
     return response.data;}
 export const findTuits  = async ()     => {const response = await axios.get(TUITS_API);
