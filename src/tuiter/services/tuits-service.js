7a0b7e97fc90d181897eb3475dd5c3f8cce7ac6a
Tarakeswar Nallamothu <59277224+tarakeswar11@users.noreply.github.com>
2023-06-14 23:37:46 -0400

    url change due to render error

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const TUITS_API = 'http://localhost:4000/api/tuits';
-const TUITS_API = "https://tuiter-node-server-app-edyt.onrender.com/api/tuits"
+const TUITS_API = "https://tuiter-node-server-app-tarak.herokuapp.com/api/tuits"
 
 
 
